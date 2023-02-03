cb5b28edb09d1d9ac64fed0837bcf773c5a60be9
Yuriy Pilipenko <yurik2061@gmail.com>
2023-02-03 23:03:55 +0200

    tags

--- js/tag-list-2.js
+++ js/tag-list-2.js
@@ -1,4 +1,7 @@
 const tagsContainer = document.querySelector('.js-tags')
+// const selectedTags = []
+const selectedTags = new Set() 
+
 
 tagsContainer.addEventListener('click',onTagsContainerClick)
 
@@ -7,6 +10,26 @@ function onTagsContainerClick(e) {
         return
     }
 
-console.log(e.target);
-e.target.classList.toggle('tags__btn--active')
+    const btn = e.target;
+    const tag = btn.dataset.value;
+    const isActive = btn.classList.contains('tags__btn--active')
+
+    if(isActive) {
+        selectedTags.delete(tag)
+    } else {
+        selectedTags.add(tag)
+    }
+
+    btn.classList.toggle('tags__btn--active')
+
+    console.log(selectedTags);
+
+// console.log(e.target);
+// e.target.classList.toggle('tags__btn--active')
+// selectedTags.add(e.target.dataset.value)
+
+// selectedTags.push(e.target.dataset.value)
+// console.log(selectedTags);
+
+
 }
