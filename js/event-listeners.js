7655be8aab06fa32a40a55d4d5ba46028b1f8acb
Yuriy Pilipenko <yurik2061@gmail.com>
2023-02-03 10:39:04 +0200

    click

---
+++ js/event-listeners.js
@@ -0,0 +1,29 @@
+const targetBtn = document.querySelector('.js-target-btn');
+const addListernerBtn = document.querySelector('.js-add-listener');
+const removeListenerBtn = document.querySelector('.js-remove-listener');
+
+targetBtn.addEventListener('click', () => {
+    console.log('KLIK');
+})
+
+addListernerBtn.addEventListener('click', () => {
+    console.log('hang');
+    targetBtn.addEventListener('click', onTargetBtnClick )
+})
+
+removeListenerBtn.addEventListener('click', () => {
+    console.log('remove');
+    removeListenerBtn.addEventListener('click', onTargetBtnClick )
+})
+
+
+
+function onTargetBtnClick() {
+    console.log('click');
+
+} 
+
+function logMessage() {
+     console.log('Target button click');
+
+}
