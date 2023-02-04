cdcaa3cbcd02842da2128eaf83a1d737b4f61010
Yuriy Pilipenko <yurik2061@gmail.com>
2023-02-04 17:24:49 +0200

    colorpicker

--- js/colorpicker.js
+++ js/colorpicker.js
@@ -2,19 +2,19 @@ function getRandomHexColor() {
     return `${Math.floor(Math.random() * 16777215)
       .toString(16)
       .padStart(6, 0)
-    }`;
-  }
+     }`;
+     }
 
-  function hexToRgb(hex){
+function hexToRgb(hex){
     let converter = [hex.slice(0, 2), hex.slice(2, 4), hex.slice(4, 6)]; //definir os intervalos para corte (FF, FF, FF)
     let keep = [];
     let i = 0;
-// console.log(converter);
+    // console.log(converter);
     
-converter.forEach(element => {
+    converter.forEach(element => {
     keep[i] = parseInt(element, 16); // estrutura para converter o valor cortado e armazenar o mesmo na posicao do vetor
     i+=1; //contador
-});
+    });
     
     return (keep.join()); //utilizar .join() no vetor para exibir os elementos do vetor juntos
     }
@@ -22,14 +22,12 @@ converter.forEach(element => {
     let hexValue  =  getRandomHexColor()
     let rgbValue = hexToRgb(hexValue)
 
-    console.log(hexValue);
-    console.log(rgbValue);
+    // console.log(hexValue);
+    // console.log(rgbValue);
     const colors = []
 
     const create = document.querySelector('.create-color');
     create.addEventListener('click',createObjectOfColors());
-
-   
 
 function createObjectOfColors(x){
    
@@ -44,9 +42,62 @@ colors.push(item);
 return colors;
 }
 
-for (let i = 1; i < 9; i+=1) {
+for (let i = 0; i < 150; i+=1) {
     createObjectOfColors()
     
 }
 
-console.log(colors);
+console.log(colors); // array of colors
+
+//===============colorpicker==================
+
+const paletteContainer = document.querySelector('.js-palette')
+const cardsMarkup = createColorCardsMarkup(colors)
+
+paletteContainer.insertAdjacentHTML('beforeend',cardsMarkup)
+paletteContainer.addEventListener('click',onPaletteContainerClick )
+
+// console.log(createColorCardsMarkup(colors))
+
+function createColorCardsMarkup (colors) {
+    const markup = colors.map(({hex,rgb}) => {
+        return `
+        <div class="color-card">
+        <div class="color-swatch"
+            data-hex="${hex}"
+            data-rgb="${rgb}"
+            style="background-color: ${hex};"
+        ></div>
+        <div class="color-meta">
+            <p>HEX: ${hex}</p>
+            <p>RGB: ${rgb}</p>
+        </div>
+    </div> 
+        `;
+    });
+console.log(markup);
+
+return markup.join('');
+// creates markup
+}
+
+function onPaletteContainerClick(e) {
+   if(!e.target.classList.contains('color-swatch'))  {
+    return
+   }
+
+  
+
+   const currentActiveCard = document.querySelector('.color-card.is-active');
+   if(currentActiveCard) {
+    currentActiveCard.classList.remove('is-active')
+   }
+   const swatchEl = e.target;
+   const parentColorCard = swatchEl.closest('.color-card') 
+
+   parentColorCard.classList.add('is-active')
+//    console.log(e.target.dataset.hex);
+//    console.log(e.target.dataset.rgb);
+   document.body.style.backgroundColor = swatchEl.dataset.hex
+}  
+
